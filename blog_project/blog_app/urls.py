b82e3445923129ba8449110b2521631560d0f85e
gega <gegatvara@gmail.com>
2022-08-07 15:12:28 +0400

    forms and user accounts

--- blog_project/blog_app/urls.py
+++ blog_project/blog_app/urls.py
@@ -1,6 +1,15 @@
-from .views import DataListView, BlogDetailView
+from .views import (
+    DataListView,
+    BlogDetailView,
+    BlogCreateView,
+    BlogUpdateView,
+    BlogDeleteView,
+)
 from django.urls import path
 urlpatterns = [
     path('', DataListView.as_view(), name="home"),
-    path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
+    path('post/<int:pk>/', BlogDetailView.as_view(), name="post_detail"),
+    path('post/new/', BlogCreateView.as_view(), name="post_new"),
+    path('post/<int:pk>/edit/', BlogUpdateView.as_view(), name="post_edit"),
+    path('post/<int:pk>/delete/', BlogDeleteView.as_view(), name="post_delete")
 ]
